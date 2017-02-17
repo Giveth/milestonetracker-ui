c4685dfe4a73e971a15be37c6e091ce5f3f0d4ad
Vojtech Simetka <vojtech@simetka.cz>
2017-02-17 08:56:40 +0100

    Prod version URL

--- dapp/js/blockchain/index.js
+++ dapp/js/blockchain/index.js
@@ -10,7 +10,7 @@ if ((window) && (window.web3)) {
     web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
 
-// const givethDirectory = new GivethDirectory(web3, "0x30e1a463ecf25dbba2f83cb3e4b10045f888e55b");
-const givethDirectory = new GivethDirectory(web3, "0xe78a0f7e598cc8b0bb87894b0f60dd2a88d6a8ab");
+const givethDirectory = new GivethDirectory(web3, "0x30e1a463ecf25dbba2f83cb3e4b10045f888e55b");
+// const givethDirectory = new GivethDirectory(web3, "0xe78a0f7e598cc8b0bb87894b0f60dd2a88d6a8ab");
 
 export { web3, MilestoneTracker, givethDirectory };
