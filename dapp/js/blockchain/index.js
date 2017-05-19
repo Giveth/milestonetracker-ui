21f363bb2272b33a638b421bb0ad878908b7f80d
Vojtech Simetka <vojtech@simetka.cz>
2017-05-19 08:45:56 +0200

    Changed the contract URL to match the main network giveth directory.

--- dapp/js/blockchain/index.js
+++ dapp/js/blockchain/index.js
@@ -12,9 +12,9 @@ if ((window) && (window.web3)) {
 }
 
 //MAIN
-//const givethDirectory = new GivethDirectory(web3, "0x30e1a463ecf25dbba2f83cb3e4b10045f888e55b");
+const givethDirectory = new GivethDirectory(web3, "0x30e1a463ecf25dbba2f83cb3e4b10045f888e55b");
 //ROPSTEN
-const givethDirectory = new GivethDirectory(web3, "0x53fc022DD190F0b37A5501FeE92171Ed1C7CD4Eb");
+// const givethDirectory = new GivethDirectory(web3, "0x53fc022DD190F0b37A5501FeE92171Ed1C7CD4Eb");
 //TESTRPC
 //const givethDirectory = new GivethDirectory(web3, "0xe78a0f7e598cc8b0bb87894b0f60dd2a88d6a8ab");
 
