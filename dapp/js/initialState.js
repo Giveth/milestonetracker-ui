2e893e3fbdd8abf838ec764eadeb8216196ec478
kkx <qijiongjiang1@hotmail.com>
2017-07-08 10:08:46 +0200

    remove trailing spaces

--- dapp/js/initialState.js
+++ dapp/js/initialState.js
@@ -3,11 +3,11 @@ module.exports = () => (
         userAccount: "",
         error: null,
         campaignValues: {
-            escapeCaller: "",  
+            escapeCaller: "",
             escapeDestination: "0x32bacc8B241FB172fEE18bDa32527126c6f3c5f7",
-            securityGuard: "", 
+            securityGuard: "",
             arbitrator: "0x32bacc8b241fb172fee18bda32527126c6f3c5f7", // Giveth Multisig
-            donor: "",  
+            donor: "",
             recipient: "",
             tokenName: "",
             tokenSymbol: "",
