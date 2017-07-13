133e8c5a7df516fb92f6a6a81dbc0eae228246e5
Vojtech Simetka <vojtech@simetka.cz>
2017-07-13 21:52:09 +0200

    Donate button should work as expected.

--- dapp/js/components/InputMyAddresses.jsx
+++ dapp/js/components/InputMyAddresses.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 
-class Input extends React.Component {
+class InputMyAddresses extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -12,7 +12,7 @@ class Input extends React.Component {
     }
 
     componentWillMount() {
-        this.inputChanged(this.props.value);
+        this.inputChanged(this.props.name);
     }
 
     onChange(event) {
@@ -20,7 +20,7 @@ class Input extends React.Component {
     }
 
     inputChanged(value) {
-        const valid = this.props.validate(value);
+        const valid = value >= 0 && value < this.props.accounts.length;
         this.props.onChange(this.props.name, value, valid);
 
         this.setState({
@@ -29,39 +29,44 @@ class Input extends React.Component {
     }
 
     render() {
+        const accounts = [ <option key="-1" value="-1">Choose an account</option> ];
+        for (let i = 0; i < this.props.accounts.length; i += 1) {
+            accounts.push(
+                <option
+                  key={i}
+                  value={i}
+                >
+                    {this.props.accounts[ i ].address}
+                </option>);
+        }
         return (
             <FormGroup validationState={this.state.validationState}>
-                {this.props.label ? <ControlLabel>{this.props.label}</ControlLabel> : ""}
+                <ControlLabel>{this.props.label}</ControlLabel>
                 <FormControl
                   name={this.props.name}
-                  componentClass={this.props.componentClass}
                   placeholder={this.props.placeholder}
+                  componentClass="select"
                   onChange={this.onChange}
-                  value={this.props.value}
-                />
+                >
+                    {accounts}
+                </FormControl>
             </FormGroup>
         );
     }
 }
 
-Input.propTypes = {
+InputMyAddresses.propTypes = {
     name: PropTypes.string.isRequired,
     placeholder: PropTypes.string,
-    label: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.bool,
-    ]),
-    componentClass: PropTypes.string,
+    label: PropTypes.string.isRequired,
     onChange: PropTypes.func.isRequired,
-    value: PropTypes.string,
-    validate: PropTypes.func.isRequired,
+    accounts: PropTypes.arrayOf(PropTypes.shape({
+        address: PropTypes.string.isRequired,
+    })).isRequired,
 };
 
-Input.defaultProps = {
+InputMyAddresses.defaultProps = {
     placeholder: "",
-    value: "",
-    componentClass: "input",
-    label: false,
 };
 
-export default Input;
+export default InputMyAddresses;
