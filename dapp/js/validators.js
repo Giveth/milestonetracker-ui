edfbb50419eab40cdf0f464aaa72275ec01861f2
rjewing <ewing.rj@gmail.com>
2017-07-17 19:07:58 +0200

    allow mixed case urls

--- dapp/js/validators.js
+++ dapp/js/validators.js
@@ -2,10 +2,12 @@ import { web3 } from "./blockchain";
 
 const websiteValidator = new RegExp([
     /^(https?:\/\/)/, // protocol
-    /((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))/, // OR ip (v4) address
-    /(:\d+)?(\/[-a-z\d%_.~+]*)*/, // port and path
-    /(\?[;&a-z\d%_.~+=-]*)?/, // query string
-    /(#[-a-z\d_]*)?$/, // fragment locater
+    /((([a-zA-Z\d]([a-zA-Z\d-]*[a-zA-Z\d])*)\.)+[a-zA-Z]{2,}|((\d{1,3}\.){3}\d{1,3}))/, // OR ip
+    // (v4)
+    // address
+    /(:\d+)?(\/[-a-zA-Z\d%_.~+]*)*/, // port and path
+    /(\?[;&a-zA-Z\d%_.~+=-]*)?/, // query string
+    /(#[-a-zA-Z\d_]*)?$/, // fragment locater
 ].map(r => r.source).join(""));
 
 /**
