595e73c8aa99e1718513c39e7534442e2e67d232
rjewing <ewing.rj@gmail.com>
2017-07-28 14:47:52 -0700

    display more campaign details to user

--- dapp/js/components/CampaignDetails.jsx
+++ dapp/js/components/CampaignDetails.jsx
@@ -6,9 +6,50 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { TableDetails } from "./";
+import { network } from "../blockchain";
 
 const CampaignDetails = (props) => {
     const general = [
+        {
+            label: "Donation / Campaign Controller Address",
+            content: (
+                <a
+                  href={`${ network.etherscan }address/${ props.campaign.token.controller }`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                    {props.campaign.token.controller}
+                </a>
+            ),
+        },
+        {
+            label: "Milestone Tracker Address",
+            content: (
+                <a
+                  href={`${ network.etherscan }address/${ props.campaign.milestoneTrackerAddress }`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                    {props.campaign.milestoneTrackerAddress}
+                </a>
+            ),
+        },
+        {
+            label: "Token Contract Address",
+            content: (
+                <a
+                  href={`${ network.etherscan }address/${ props.campaign.tokenAddress }`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                    {props.campaign.tokenAddress }
+                </a>
+            ),
+        },
+        {
+            label: "Token Name",
+            content: `${ props.campaign.token.name }`,
+        },
         {
             label: "Website",
             content: (<a
@@ -33,6 +74,12 @@ CampaignDetails.propTypes = {
     campaign: PropTypes.shape({
         description: PropTypes.string.isRequired,
         url: PropTypes.string.isRequired,
+        tokenAddress: PropTypes.string.isRequired,
+        token: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            controller: PropTypes.string.isRequired,
+        }),
+        milestoneTrackerAddress: PropTypes.string.isRequired,
     }).isRequired,
 };
 
