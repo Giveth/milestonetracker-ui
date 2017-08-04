f37080d8b6e0c3625cfc086b12505244d2be35f4
rjewing <ewing.rj@gmail.com>
2017-08-04 10:37:26 -0700

    disable eslint for env.js

--- env.js
+++ env.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const Web3 = require("web3");
 // create an instance of web3 using the HTTP provider.
 // NOTE in mist web3 is already available, so check first if its available before instantiating
